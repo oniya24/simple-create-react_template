4eee9c9d1f5ba75a04ffec291ac1747a45f85f74
oniya24 <www.1092031665@qq.com>
2020-04-21 21:20:49 +0800

    增加了可选配置模块
    以下模块可选：
    axios
    redux
    react-redux
    react-router-dom

--- src/component/HelloWorld.js
+++ src/component/HelloWorld.js
@@ -1,15 +1,4 @@
 import React, { Component } from 'react';
-console.log({
-    "bugs": {
-        "url": "https://github.com/oniya24/sample-create-react_template/issues"
-      },
-    "homepage": "https://github.com/oniya24/sample-create-react_template#readme",
-    "repository":{
-        "type": "git",
-        "url": "git+https://github.com/oniya24/sample-create-react_template.git"
-    },
-    },
-)
 class HelloWorld extends Component{
     constructor(props){
         super(props);
